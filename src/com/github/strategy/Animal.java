e3317bf2f3ac6f8b3a494cc8db17cbd7804e1b7f
Harmish Lakhani <harmish.lakhani@gmail.com>
2016-05-12 12:12:39 +0530

    added info and imp links for strategy

--- src/com/github/strategy/Animal.java
+++ src/com/github/strategy/Animal.java
@@ -1,9 +1,33 @@
 package com.github.strategy;
 
 /**
- * NOTES:
- * Avoid Interfaces that just force action
- * Using Interfaces just to force the method creation is bad idea
+ * TYPE : Behavioral
+ * 
+ * NOTES: Avoid Interfaces that just force action, Using Interfaces just to
+ * force the method creation is bad idea
+ * 
+ * The strategy pattern uses composition instead of inheritance. In the strategy
+ * pattern, behaviors are defined as separate interfaces and specific classes
+ * that implement these interfaces. This allows better decoupling between the
+ * behavior and the class that uses the behavior. The behavior can be changed
+ * without breaking the classes that use it, and the classes can switch between
+ * behaviors by changing the specific implementation used without requiring any
+ * significant code changes. Behaviors can also be changed at run-time as well
+ * as at design-time
+ * 
+ * This gives greater flexibility in design and is in harmony with the
+ * Open/closed principle (OCP) that states that classes should be open for
+ * extension but closed for modification.
+ * 
+ * Identify the aspects of your application that vary and separate them from
+ * what stays the same. Take what varies and “encapsulate” it so it won’t affect
+ * the rest of your code. 
+ * Program to an interface not to an implementation.
+ * Favor composition over inheritance.
+ * 
+ * Links:
+ * https://en.wikipedia.org/wiki/Strategy_pattern
+ * https://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Strategy
  * 
  */
 public class Animal {
